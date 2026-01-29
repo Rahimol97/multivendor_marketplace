eed47ca47e30e99bb663cc699f82937bbfe77027
rahi <rahimolrg@gmail.com>
2026-01-29 21:18:20 +0530

    error fix

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ connectDB();
 const app = express();
 app.use(cookieParser());
 app.use(cors({
-  origin:"http://multivendor-marketplace-rouge.vercel.app/",
+  origin:"http://multivendor-marketplace-rouge.vercel.app",
   credentials: true
 })); 
 
