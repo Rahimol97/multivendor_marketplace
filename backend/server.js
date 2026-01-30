6b83b9124fdc4e279c4f236c73f59e205cdaec9a
rahi <rahimolrg@gmail.com>
2026-01-30 14:19:37 +0530

    backend url

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ connectDB();
 const app = express();
 app.use(cookieParser());
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"http://multivendor-marketplace-qvud.onrender.com",
   credentials: true
 })); 
 
