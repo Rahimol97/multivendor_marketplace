a80bf73c271c30c40f9852fede5d625fcddd958e
rahi <rahimolrg@gmail.com>
2026-02-03 11:13:06 +0530

    error server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ connectDB();
 const app = express();
 app.use(cookieParser());
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://multivendor-marketplace-flax.vercel.app",
   credentials: true
 })); 
 
